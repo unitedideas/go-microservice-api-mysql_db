4c1c0b8414754f26269082c9fef8dfc2cd68bdb0
unitedideas <unitedideas@gmail.com>
2019-07-09 09:37:13 -0700

    Value and Pointer receivers on structs

--- app/app.go
+++ app/app.go
@@ -25,29 +25,41 @@ const usixteenbitmax float64 = 65535
 const khm_multiple float64 = 1.60934
 
 type car struct {
-	gas_peddle     uint16 // min 0, max 65535
-	brake_peddle   uint16
-	steering_wheel int16 // -32k , +32k
-	top_speed_kmh  float64
+	gasPeddle     uint16 // min 0, max 65535
+	brakePeddle   uint16
+	steeringWheel int16 // -32k , +32k
+	topSpeedKmh   float64
 }
 
+// Value receiver makes a copy of the struct we passed to it so the value of the
+// original type is not modified
+
 func (c car) kmh() float64 {
-	return float64(c.gas_peddle) * (c.top_speed_kmh / usixteenbitmax)
+	return float64(c.gasPeddle) * (c.topSpeedKmh / usixteenbitmax)
 }
 
 func (c car) mph() float64 {
-	return float64(c.gas_peddle) * (c.top_speed_kmh / usixteenbitmax / khm_multiple)
+	return float64(c.gasPeddle) * (c.topSpeedKmh / usixteenbitmax / khm_multiple)
+}
+
+// Pointer receiver points to the value in memory and modifies the value
+func (c *car) newTopSpeed(newSpeed float64) {
+	c.topSpeedKmh = newSpeed
 }
 
 func main() {
-	a_car := car{
-		gas_peddle:     65000,
-		brake_peddle:   0,
-		steering_wheel: 12561,
-		top_speed_kmh:  225.0}
+	aCar := car{
+		gasPeddle:     65000,
+		brakePeddle:   0,
+		steeringWheel: 12561,
+		topSpeedKmh:   225.0}
 
-	fmt.Println(a_car.gas_peddle)
-	fmt.Println(a_car.kmh())
-	fmt.Println(a_car.mph())
+	fmt.Println(aCar.gasPeddle)
+	fmt.Println("kmh:", aCar.kmh())
+	fmt.Println("mph:", aCar.mph())
+	fmt.Println()
+	aCar.newTopSpeed(500)
+	fmt.Println("kmh:", aCar.kmh())
+	fmt.Println("mph:", aCar.mph())
 
 }
